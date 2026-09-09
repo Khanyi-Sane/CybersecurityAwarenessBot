cad1a3bcceda5f4e02ea0b8dfa5b283e7c98bee7
Sanele Khanyisile Mhlanga <ST10498540@rcconnect.edu.za>
2026-09-09 18:03:29 +0200

    Add UserProfile class

---
+++ CybersecurityAwarenessBot/UserProfile.cs
@@ -0,0 +1,9 @@
+﻿// UserProfile.cs
+// Stores basic information about the person chatting to the bot.
+// The Name property allows the chatbot to save and use the user's name.
+
+public class UserProfile
+{
+    // Stores the user's name.
+    public string Name { get; set; } = "";
+}
